8a8b1a634b7e2a88d9da2e75ef68e026f5f51cdd
soboniek <olga.lepecka@gmail.com>
2020-10-28 22:49:44 +0100

    templates, css, js

--- GiveAwayApp/views.py
+++ GiveAwayApp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LandingPageView(TemplateView):
+    template_name = 'index.html'
+
+
+class AddDonationView(TemplateView):
+    template_name = 'form.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+
+class RegisterView(TemplateView):
+    template_name = 'register.html'
+
+# utwórz 4 proste widoki:
+#
+#     LandingPage
+#
+#     AddDonation
+#
+#     Login
+#
+#     Register
+#     niech każdy obsługuje tylko metodę get i renderuje odpowiedni szablon html
+#
